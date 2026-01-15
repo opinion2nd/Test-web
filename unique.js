6c7ea98de88c31fed08a15b94465e889eaf61c67
opinion2nd <hakaistan@gmail.com>
2026-01-15 15:22:12 +0600

    Update unique.js

--- unique.js
+++ unique.js
@@ -1,5 +1,16 @@
-function startExperience() {
-  const music = document.getElementById("bgMusic");
-  music.volume = 0.6;
+const music = document.getElementById("bgMusic");
+const btn = document.getElementById("playBtn");
+
+btn.onclick = () => {
   music.play();
+  btn.style.display = "none";
+};
+
+// Wind particles
+const wind = document.querySelector(".wind");
+for(let i=0;i<25;i++){
+  const span=document.createElement("span");
+  span.style.top=Math.random()*100+"%";
+  span.style.animationDelay=Math.random()*6+"s";
+  wind.appendChild(span);
 }
